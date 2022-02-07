3bb8827bda59217260d7a18da25311aed87e590e
DESKTOP-55DD24E\smart grass <nasa@battlecryhq.cn>
2022-02-07 20:24:01 +0800

    fix

--- AutoPcr_py/pyPipInstall.cmd
+++ AutoPcr_py/pyPipInstall.cmd
@@ -1,3 +1,5 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple opencv-python
+
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple aircv
 
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyautogui
