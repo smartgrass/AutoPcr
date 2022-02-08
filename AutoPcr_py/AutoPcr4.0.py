657ae7b84b7d60c0de3538d5e967b5c55c460978
DESKTOP-55DD24E\smart grass <nasa@battlecryhq.cn>
2022-02-09 02:27:09 +0800

    update

--- AutoPcr_py/AutoPcr4.0.py
+++ AutoPcr_py/AutoPcr4.0.py
@@ -1,6 +1,7 @@
 
 import sys
 from sysconfig import get_path
+from tkinter import E
 from tkinter.tix import Tree
 from xmlrpc.client import Boolean
 import PySimpleGUI as sg
@@ -35,7 +36,7 @@ if(os.path.exists(GetImagPath('config.ini')) == False):
 	curDir =os.getcwd()
 
 waitTime = 0
-minMatch = 0.64 #最低相似度匹配
+minMatch = 0.7 #最低相似度匹配
 warnMatch = 0.85 #相似度小于此时, 打印黄字
 dxcIndex =1
 StartBossIndex = 0
@@ -115,6 +116,9 @@ def ToFightPage():
 def ToHomePage():
 	if(IsHasImg("home2.png")==False):
 		WaitToClickImg("home.png",True,True,5)
+	time.sleep(1)
+def ToShopPage():
+	WaitToClickImg("shop/shop1.png",True,True,5)
 	time.sleep(1)
 
 #按钮事件
@@ -353,6 +357,38 @@ def WaitBossFight():
 		StartBoss()
 
 
+def BuyExp():
+	ToShopPage()
+	WaitToClickImg('shop/select1.png')
+	if(IsHasImg('shop/exp2.png',False) == False):
+		ToHomePage()
+		print('no to buy')
+		return
+	expCounter = 1
+	while((expCounter < 4) & (IsHasImg('shop/exp.png'))):
+		expCounter = expCounter+1
+		print('IsHasImg' ,expCounter)
+	WaitToClickImg('shop/buyBtn.png')
+	WaitToClickImg('shop/buyTitle.png',False)
+	WaitToClickImg('tansuo/sure.png')
+	time.sleep(0.5)
+	WaitToClickImg('tansuo/sure.png')
+	ToHomePage()
+
+def NiuDan():
+	WaitToClickImg('niuDan.png')
+	time.sleep(1)
+	DoKeyDown('6')
+	if(IsHasImg('other/niu2.png')):
+		ToHomePage()
+	else:
+		print()
+
+def SendZb():
+	print()
+	ToHomePage()
+	WaitToClickImg('other/hanghui.png')
+
 def LongTimeCheck(im1,im2):
 	isWaiting = True
 	#True表示识别1图 False表示识别2图
@@ -392,11 +428,20 @@ def AutoHuoDong():
 	AutoHuoDong()
 
 def DailyTasks():
-	StartTanSuo()
-	StartJJC()
-	StartPJJC()
-	StartDxc(1)
-	StartTakeAll()
+	if(isSend):
+		SendZb()
+	if(isNiuDan):
+		NiuDan()
+	if(isExp):
+		BuyExp()
+	if(isTansuo):
+		StartTanSuo()
+	if(isJJC):
+		StartJJC()
+		StartPJJC()
+	if(isDxc):
+		StartDxc(1)
+		StartTakeAll()
 
 def _async_raise(tid, exctype):
     tid = ctypes.c_long(tid)
@@ -443,6 +488,10 @@ nextKey = 'n' #n 是下一步
 endKey ='Esc'
 role3Key = 'm' #春黑是第个三位置
 
+StartRunName = "启动模拟器并运行"
+RunName = "运行"
+
+
 
 def RunAutoPcr():
 	#按下Esc键停止
@@ -452,12 +501,14 @@ def RunAutoPcr():
 	t0.start()
 	t1 = threading.Thread(target=LoopKeyDown,args=(role3Key,))
 	time.sleep(0.5)
-	WaitStart()
+	if(isRunAndStart):
+		WaitStart()
 	print('=== Start ===')
 	# WaitImgLongTime("jjc/jjcTop.png") #用于检测匹配程度,用jjcTop
 	# LongTimeCheck("dxc/box3.png","dxc/box4.png")
 	# AutoHuoDong()  #打普通关用
 	# StartBoss()
+
 #日常
 	DailyTasks()
 	print('=== end ===')
@@ -466,11 +517,31 @@ def RunAutoPcr():
 cfg = ConfigParser()
 configPath = GetImagPath('config.ini')
 cfg.read(configPath)
-isJJC = Boolean(cfg.get('MainSetting','isJJC')) # 返回1
+isRunAndStart = False
+isJJC = Boolean(cfg.get('MainSetting','isJJC')=='True')
+isTansuo = Boolean(cfg.get('MainSetting','isTansuo')=='True')
+isDxc = Boolean(cfg.get('MainSetting','isDxc')=='True')
+isExp = Boolean(cfg.get('MainSetting','isExp')=='True')
+isSend = Boolean(cfg.get('MainSetting','isSend')=='True')
+isNiuDan = Boolean(cfg.get('MainSetting','isNiuDan')=='True')
 
+def SetCurConfig(AllValues):
+	global isJJC,isTansuo,isExp,isDxc,isSend,isNiuDan
+	isJJC = AllValues[0]
+	isTansuo =AllValues[1]
+	isDxc=AllValues[2]
+	isExp=AllValues[3]
+	isSend=AllValues[4]
+	isNiuDan = AllValues[5]
 #保存配置
 def SavaConfig(AllValues):
-	cfg.set('MainSetting', 'isJJC', str(AllValues[0])) # 即可以修改已有的配置，也可以添加新的配置文件
+	cfg.set('MainSetting', 'isJJC', str(AllValues[0]))
+	cfg.set('MainSetting', 'isTansuo', str(AllValues[1]))
+	cfg.set('MainSetting', 'isDxc', str(AllValues[2]))
+	cfg.set('MainSetting', 'isExp', str(AllValues[3]))
+	cfg.set('MainSetting', 'isSend', str(AllValues[4]))
+	cfg.set('MainSetting', 'isNiuDan', str(AllValues[5]))
+
 	with open(configPath, "w+") as f:
 		cfg.write(f)
 # isJJC = True
@@ -478,9 +549,9 @@ def SavaConfig(AllValues):
 # sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 # [sg.Text('Enter something on Row 2'), sg.InputText()],
-layout = [  [sg.Checkbox('竞技场',isJJC)],[sg.Checkbox('探索',True)],[sg.Checkbox('地下城',True)],
-			[sg.Checkbox('购买mana',True)],[sg.Checkbox('送装备',True)],
-			[sg.Button('StartAndRun'), sg.Button('Run'),sg.Button('Save'), ] ]
+layout = [  [sg.Checkbox('竞技场',isJJC)],[sg.Checkbox('探索',isTansuo)],[sg.Checkbox('地下城',isDxc)],
+			[sg.Checkbox('购买经验',isExp)],[sg.Checkbox('送装备',isSend)],[sg.Checkbox('扭蛋',isNiuDan)],
+			[sg.Button(StartRunName), sg.Button(RunName),sg.Button('Save'), ] ]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
@@ -489,9 +560,12 @@ while True:
 	event, values = window.read()
 	if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
 		break
+	SetCurConfig(values)
 	if event == 'Save':
 		SavaConfig(values)
-	if event == 'StartAndRun':
+	if ((event == StartRunName) | (event == RunName)):
+		isRunAndStart = (event == StartRunName)
+		print('Run ', isRunAndStart )
 		tMain = threading.Thread(target=RunAutoPcr)
 		tMain.start()
 	# print('You entered ', values[0])
---
+++ AutoPcr_py/moniq.cmd
@@ -0,0 +1,5 @@
+cd /d D:\program files\LeiDian\LDPlayer4.0
+
+dnconsole launch --index 1
+
+exit
