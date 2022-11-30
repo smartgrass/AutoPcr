690ef3b7d2766ad93b0a8c16afbe20e67153c1ec
DESKTOP-6P4QJO8\Nasa <nasa@battlecryhq.com>
2022-11-30 18:26:36 +0800

    setupPip备份

---
+++ AutoPcr_py/SetupPip_1.cmd
@@ -0,0 +1,29 @@
+python.exe -m pip install --upgrade pip
+
+pip install opencv-python
+
+pip install aircv
+
+pip install pyautogui
+
+pip install pillow
+
+pip install keyboard
+
+pip install pypiwin32
+
+pip install PySimpleGUI
+
+set path=%~dp0xiaocao.vbs
+
+cd %~dp0
+
+echo %path%
+
+start %path%
+
+echo create lnk
+
+echo OK!!
+
+pause
